b7c2905e2e7ec45e90ca1828e0cf918a499a1a67
Carlos Peixe <carlos.peixe@georgiancollege.ca>
2019-07-05 19:30:16 -0400

    Login page done

---
+++ AuthApp/Models/AccountDB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AuthApp.Models;
+
+namespace AuthApp.Models
+{
+    public class AccountDB
+    {
+        private List<User> usersList = new List<User>();
+
+        public AccountDB()
+        {
+            usersList.Add(new User { Id = 1, Username = "noAccess", Password = "123", Roles = new string[] { }, LoggedIn = false });
+            usersList.Add(new User { Id = 2, Username = "user", Password = "123", Roles = new string[] {"user" }, LoggedIn = false });
+            usersList.Add(new User { Id = 3, Username = "admin", Password = "123", Roles = new string[] {"admin", "user" }, LoggedIn = false });
+        }
+
+        public User find(string username)
+        {
+            return usersList.Where(u => u.Username.Equals(username)).FirstOrDefault();
+        } 
+
+        public User login(string username, string password)
+        {
+            return usersList.Where(u => u.Username.Equals(username) && u.Password.Equals(password)).FirstOrDefault();
+        }
+    }
+}
---
+++ AuthApp/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AuthApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Please inform username")]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage ="Please inform password")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        public string[] Roles { get; set; }
+
+        public bool LoggedIn { get; set; }
+    }
+
+   
+}
